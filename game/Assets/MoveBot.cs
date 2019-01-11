5c2fc874fa8c45b838ab7c73fbbb6f87cd788274
InsideOutside0 <sammy@salwen.org>
2019-01-11 01:20:29 -0800

    Adds comments and starts collisions

--- game/Assets/MoveBot.cs
+++ game/Assets/MoveBot.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class MoveBot : MonoBehaviour {
 
-    [SerializeField] private float deltaAngle = 0.1f;
+    [SerializeField] private float rotationSpeed = 0.1f;
     [SerializeField] private float acceleration = 1f;
     [SerializeField] private float slowdown = 1.5f; // I refuse to call it "deceleration"
     [SerializeField] private float velocitCap = 3f;
 
-    private Rigidbody2D rb;
+    private Rigidbody2D rb; // this is what handles velocity and collisions and such
 
     private float hMove;
     private float vMove;
-    private Vector3 velocityVector;
+    private Vector2 velocityVector;
     private float velocity;
     private float angle = 0f;
     private bool goingForward = false;
@@ -22,8 +22,8 @@ public class MoveBot : MonoBehaviour {
     // Start is called before the first frame update
     void Start()
     {
-        velocityVector = new Vector3();
-        rb = GetComponent<Rigidbody2D>();
+        velocityVector = new Vector2();
+        rb = GetComponent<Rigidbody2D>(); // you cannot use a RigidBody without initializing it first
     }
 
     // Update is called once per frame
@@ -31,7 +31,7 @@ public class MoveBot : MonoBehaviour {
     {
         hMove = Input.GetAxisRaw("Horizontal");
         vMove = Input.GetAxisRaw("Vertical");
-        angle = transform.rotation.eulerAngles.z;
+        angle = transform.rotation.eulerAngles.z; // the z angle is the effective angle in a 2D space
     }
 
 
@@ -39,15 +39,17 @@ public class MoveBot : MonoBehaviour {
 
     private void Move() // uses vertical input
     {
-        if (vMove>0) {
+        if (vMove>0) { // up input
+            // use the bot's angle to determine movemennt direction
             goingForward = true;
             if (velocity<velocitCap) {
                 velocity += acceleration;
                 velocity = Clamp(velocity, 0, velocitCap);
             }
-            velocityVector = new Vector3(velocity*Mathf.Cos(toRadians(angle)), velocity*Mathf.Sin(toRadians(angle)), 0);
+            velocityVector = new Vector2(velocity*Mathf.Cos(toRadians(angle)), velocity*Mathf.Sin(toRadians(angle)));
 
         } else if (vMove<0) {
+
             if (goingForward) {
                 velocity -= slowdown;
                 velocity = Clamp(velocity, 0f, velocitCap);
@@ -55,20 +57,22 @@ public class MoveBot : MonoBehaviour {
             } else {
                 velocity += acceleration;
                 velocity = Clamp(velocity, 0f, velocitCap);
-                velocityVector = new Vector3(-velocity*Mathf.Cos(toRadians(angle)), -velocity*Mathf.Sin(toRadians(angle)), 0);
+                velocityVector = new Vector2(-velocity * Mathf.Cos(toRadians(angle)), -velocity * Mathf.Sin(toRadians(angle)));
             }
 
         } else {
+
             velocity -= slowdown;
             velocity = Clamp(velocity, 0, velocitCap);
             if (velocity == 0) goingForward = false;
-            velocityVector = new Vector3(velocity * Mathf.Cos(toRadians(angle)), velocity * Mathf.Sin(toRadians(angle)), 0);
+            velocityVector = new Vector2(velocity * Mathf.Cos(toRadians(angle)), velocity * Mathf.Sin(toRadians(angle)));
+
         }
 
-        rb.velocity = new Vector2(velocityVector.x, velocityVector.y);
-
+        rb.velocity = velocityVector;
     }
 
+    // this function ensures that a number is within a given range
     private float Clamp(float num, float min, float max)
     {
         if (num > max) return max;
@@ -76,20 +80,22 @@ public class MoveBot : MonoBehaviour {
         return num;
     }
 
+    // Unity uses degrees. That is a disgrace, though I understand the descision.
     private float toRadians(float num) {
         return num*Mathf.PI/180;
     }
 
     private void Rotate() // uses horizontal input
     {
-        if (hMove>0)
+        if (hMove>0) // right input
         {
             // Vector3.forward = (0, 0, 1)
-            transform.Rotate(Vector3.forward * -deltaAngle);
+            // transform.Rotate rotates BY the angle of the parameter, not TO the angle
+            transform.Rotate(Vector3.forward * -rotationSpeed);
         }
-        else if (hMove<0)
+        else if (hMove<0) // left input
         {
-            transform.Rotate(Vector3.forward * deltaAngle);
+            transform.Rotate(Vector3.forward * rotationSpeed);
         }
     }
 
---
+++ game/Assets/RobotActions.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotActions : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collision col)
+    {
+        if (col.gameObject.name.Equals("TestBall"))
+        {
+            col.gameObject.transform.parent = transform;
+        }
+    }
+}
