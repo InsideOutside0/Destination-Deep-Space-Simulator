45e36115445ddfb1f9d424e887114c0d2b73cdf9
InsideOutside0 <sammy@salwen.org>
2019-01-15 01:22:50 -0800

    Adds different menu types that cannot be accessed because collisions don't work

--- game/Assets/Scripts/MenuController.cs
+++ game/Assets/Scripts/MenuController.cs
@@ -4,31 +4,92 @@ using UnityEngine;
 
 public class MenuController : MonoBehaviour
 {
-
+    const float z = -0.1f;
     public Transform button;
 
     // Start is called before the first frame update
     void Start()
     {
-        loadMainMenu();
+        LoadMainMenu();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        OnClick();
     }
 
-    void createButton(string s, Vector3 position)
+    void CreateButton(string s, Vector3 position)
     {
-        Transform newButton = Instantiate(button, position, Quaternion.identity);
+        Transform newButton = Instantiate(button, position, Quaternion.identity /* Indicates no rotation */ );
         newButton.GetComponentInChildren<TextMesh>().text = s;
+        newButton.name = s;
+        newButton.tag = "button";
     }
 
-    void loadMainMenu()
+    void RemoveAllButtons()
     {
-        createButton("PLAY", new Vector3(0, 0.5f, -0.1f));
-        createButton("HELP", new Vector3(0, -1.5f, -0.1f));
-        createButton("QUIT", new Vector3(0, -3.5f, -0.1f));
+        GameObject[] buttons = GameObject.FindGameObjectsWithTag("button");
+        foreach (GameObject b in buttons)
+        {
+            Object.Destroy(b);
+        }
+    }
+
+    void LoadMainMenu()
+    {
+        RemoveAllButtons();
+        CreateButton("PLAY", new Vector3(0, 1f, z));
+        CreateButton("HELP", new Vector3(0, -0.5f, z));
+        CreateButton("OPTIONS", new Vector3(0, -2f, z));
+        CreateButton("QUIT", new Vector3(0, -3.5f, z));
+    }
+
+    void LoadHelp()
+    {
+        RemoveAllButtons();
+        CreateButton("BACK", new Vector3(0, -3.5f, z));
+    }
+
+    void LoadOptions()
+    {
+        RemoveAllButtons();
+        CreateButton("WIP", new Vector3(0, 1f, z));
+        CreateButton("BACK", new Vector3(0, -3.5f, z));
+    }
+
+    void OnClick()
+    {
+        if (Input.GetMouseButtonDown(0)) // if left click
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+
+            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+            // dost thou light that burneth the sky shineth on mine button?
+            if (hit.collider != null)
+            {
+                print("holy shit");
+                /*switch (hit.transform.name)
+                {
+                    case "PLAY":
+                        break;
+                    case "HELP":
+                        LoadHelp();
+                        break;
+                    case "OPTIONS":
+                        LoadOptions();
+                        break;
+                    case "QUIT":
+                        Application.Quit();
+                        break;
+                    case "BACK":
+                        LoadMainMenu();
+                        break;
+                    default: break;
+                }*/
+
+            }
+        }
     }
 }
