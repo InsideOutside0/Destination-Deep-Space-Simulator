56055383d48e9a41206e7bb6db521c97f4c01db9
InsideOutside0 <sammy@salwen.org>
2019-01-16 17:26:06 -0800

    Continues configuration process

--- game/Assets/Scripts/RobotActions.cs
+++ game/Assets/Scripts/RobotActions.cs
@@ -5,6 +5,33 @@ using UnityEngine;
 public class RobotActions : MonoBehaviour
 {
 
+    private string cargoLevel;
+    private string panelLevel;
+    private bool hasVision;
+    private bool hasRamp;
+    private bool hasSideAuto;
+    private bool hasCenterAuto;
+
+    private float lowCargoAcc;
+    private float medCargoAcc;
+    private float highCargoAcc;
+    private float lowPanelAcc;
+    private float medPanelAcc;
+    private float highPanelAcc;
+    private float sideAutoAccMod;
+    private float centerAutoAccMod;
+
+    private float lowCargoSpeed;
+    private float medCargoSpeed;
+    private float highCargoSpeed;
+    private float lowPanelSpeed;
+    private float medPanelSpeed;
+    private float highPanelSpeed;
+    private float sideAutoSpeedMod;
+    private float centerAutoSpeedMod;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
